549c1a5e8f498ba6630a61b7699dceefca1fdb6a
Аушев Константин <echo@dev.null>
2025-05-15 18:07:03 +0300

    Добавьте файлы проекта.

---
+++ GeneratorsLib/MySourceGenerator.cs
@@ -0,0 +1,27 @@
+using Microsoft.CodeAnalysis;
+
+namespace GeneratorsLib;
+
+[Generator]
+public class MySourceGenerator : ISourceGenerator
+{
+    public void Execute(GeneratorExecutionContext context)
+    {
+        // определяем генерируемый код
+        var code = @"
+            namespace Metanit
+            {
+                public static class Welcome 
+                {
+                    public const string Name = ""Eugene"";
+                    public static void Print() => Console.WriteLine($""Hello {Name}!"");
+                }
+            }";
+        context.AddSource("metanit.welcome.generated.cs", code);
+    }
+
+    public void Initialize(GeneratorInitializationContext context)
+    {
+        // инициализация не нужна
+    }
+}
